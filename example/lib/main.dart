cfb1e35c561af05467142a12f73413d6739f3773
Gautier de Lataillade <gautier2406@gmail.com>
2024-05-14 17:36:14 +0200

    Make API Changes compatible with Flutter 3.22.0

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -49,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 },
                 snapToMins: 5.0,
               ),
-            )
+            ),
           ],
         ),
       ),
